3f84df915f0c687aff65514f3f4c830584a5a952
cagayakemiracl <hirosnonno1182@gmail.com>
2014-05-11 12:03:20 +0900

    オプション引数を使えるようにする下準備

--- test_bench2.py
+++ test_bench2.py
@@ -1,6 +1,17 @@
 #!/usr/bin/env python2.7
-from test_bench_core2 import Test_bench
-from sys import argv
+# -*- coding: utf-8 -*-
 
-argv.pop(0)
-Test_bench(argv).run()
+from test_bench_core2 import Test_bench
+import argparse
+
+parser = argparse.ArgumentParser(description='''verilogのテストベンチファイルを自動生成するスクリプトです
+https://github.com/cagayakemiracl/verilog_test_bench_auto
+''')
+parser.add_argument('file_list', nargs='*', help='コンパイル時に渡すファイル')
+parser.add_argument('-s', '--topmodule', nargs='?', default='', help='テストベンチを生成したいモジュール')
+parser.add_argument('-i', '--input', nargs='?', default='', help='テストベンチを生成したいモジュールを含むファイル')
+parser.add_argument('-o', '--output', nargs='?', default='', help='テストベンチファイル名')
+parser.add_argument('-v', '--version', action='version', version='test_bench 0.1')
+args = parser.parse_args()
+
+Test_bench(args.file_list).run()
--- test_bench3.py
+++ test_bench3.py
@@ -1,6 +1,16 @@
 #!/usr/bin/env python3
+# -*- coding: utf-8 -*-
 from test_bench_core3 import Test_bench
-from sys import argv
+import argparse
 
-argv.pop(0)
-Test_bench(argv).run()
+parser = argparse.ArgumentParser(description='''verilogのテストベンチファイルを自動生成するスクリプトです
+https://github.com/cagayakemiracl/verilog_test_bench_auto
+''')
+parser.add_argument('file_list', nargs='*', help='コンパイル時に渡すファイル')
+parser.add_argument('-s', '--topmodule', nargs='?', default='', help='テストベンチを生成したいモジュール')
+parser.add_argument('-i', '--input', nargs='?', default='', help='テストベンチを生成したいモジュールを含むファイル')
+parser.add_argument('-o', '--output', nargs='?', default='', help='テストベンチファイル名')
+parser.add_argument('-v', '--version', action='version', version='test_bench 0.1')
+args = parser.parse_args()
+
+Test_bench(args.file_list).run()
