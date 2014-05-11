c4342d3669a88288c92b428ed039607a22d05907
cagayakemiracl <hirosnonno1182@gmail.com>
2014-05-11 14:46:30 +0900

    説明不足

--- test_bench2.py
+++ test_bench2.py
@@ -10,7 +10,7 @@ https://github.com/cagayakemiracl/verilog_test_bench_auto
 parser.add_argument('file_list', nargs='*', help='コンパイル時に渡すファイル')
 parser.add_argument('-s', '--topmodule', nargs='?', default='', help='テストベンチを生成したいモジュール')
 parser.add_argument('-i', '--input', nargs='?', default='', help='テストベンチを生成したいモジュールを含むファイル')
-parser.add_argument('-o', '--output', nargs='?', default='', help='テストベンチファイル名')
+parser.add_argument('-o', '--output', nargs='?', default='', help='テストベンチファイル名又はディレクトリ')
 parser.add_argument('-p', '--path', nargs='?', default='', help='iverilogなどのツールがあるディレクトリ')
 parser.add_argument('-v', '--version', action='version', version='test_bench 0.1')
 args = parser.parse_args()
